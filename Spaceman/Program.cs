9a74403bfa0d0f92604cceb739dec99f151a5e97
anna-dahlberg <anna.dahlberg@live.se>
2025-04-19 19:10:48 +0200

    wrote program, not yet working as wished

--- Spaceman/Program.cs
+++ Spaceman/Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace Spaceman
-
-class Program
 {
-    static void Main(string[] args)
+    class Program
     {
-
+        static void Main(string[] args)
+        {
+            Game game = new Game(); 
+            game.Greet(); 
+            
+            while (!game.DidWin() && !game.DidLose())
+            {
+                game.Display(); 
+                game.Ask(); 
+            }
+            
+            if (game.DidWin())
+            {
+                Console.WriteLine("Congratulations! You've cracked the code and stopped the abduction!");
+            }
+            else
+            {
+                Console.WriteLine("Game over! The abduction was completed. Better luck next time!");
+            }
+        }
     }
 }
