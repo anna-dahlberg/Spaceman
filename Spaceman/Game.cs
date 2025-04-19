8d4c9ed36c5f11bf6834769af051c87c1384cfa5
anna-dahlberg <anna.dahlberg@live.se>
2025-04-19 17:25:04 +0200

    Greet method added

--- Spaceman/Game.cs
+++ Spaceman/Game.cs
@@ -2,5 +2,10 @@ namespace Spaceman;
 
 public class Game
 {
-    
+    public void Greet()
+    {
+        Console.WriteLine("Welcome to Spaceman!");
+        Console.WriteLine("Try to guess the word before the alien takes you.");
+        Console.WriteLine("Good luck, earthling!\n");
+    }
 }
